e26511072342991220ef7b823548f9a3f85f1cfb
Claire Chu(朱晏禛) <makoto357@gmail.com>
2024-03-14 01:24:41 +0800

    serialize and deserialize data

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const Schema = require("./kalidokit_pb");
 
+// using accessors (getters and setters methods) to access and update your data
 const lefthand = new Schema.AvatarLeftHand();
 const vec3 = new Schema.Vec3();
-vec3.setX(1);
+vec3.setX(5);
 vec3.setY(2);
 vec3.setZ(3);
 
 lefthand.setLeftwrist(vec3);
 console.log(lefthand.getLeftwrist().getX());
+
+// serialize data
+const serializedData = vec3.serializeBinary();
+console.log(serializedData);
+
+// deserialize data
+const deserializedMessage = Schema.Vec3.deserializeBinary(serializedData);
+console.log(deserializedMessage);
